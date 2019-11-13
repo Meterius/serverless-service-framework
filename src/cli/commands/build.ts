3946adce70b71e4e770c7e6bfa0911d6d56eafbd
Jonah Hoff <>
2019-11-13 13:28:34 +0100

    used new framework functions to re-implement build

--- src/cli/commands/build.ts
+++ src/cli/commands/build.ts
@@ -1,37 +1,13 @@
 import { GluegunCommand, GluegunToolbox } from "gluegun";
 import { getOption, requireParameters } from "../utility/options";
-import {
-  getFrameworkSchemaFilePath,
-  getServiceSchemaFileByName,
-  loadFrameworkSchemaFile,
-  loadServiceSchemaFiles,
-} from "../../framework/schema-handling";
-import {
-  buildServiceServerlessTemplate,
-} from "../../framework/template-handling";
-import { CliError } from "../utility/exceptions";
+import { buildService, loadFrameworkSchemas } from "../utility/framework";
 
 const build: GluegunCommand = {
   name: "build",
   description: "Builds serverless template for service",
   run: async (tb: GluegunToolbox): Promise<void> => {
-    const [serviceName] = requireParameters(tb, "service-name");
-    const schemaFilePath = getOption(tb, "schema")
-      || await getFrameworkSchemaFilePath(process.cwd());
-
-    if (schemaFilePath === undefined) {
-      throw new CliError("Didn't find framework schema file");
-    }
-
-    const frFile = await loadFrameworkSchemaFile(schemaFilePath);
-    const seFiles = await loadServiceSchemaFiles(frFile);
-    const seFile = getServiceSchemaFileByName(seFiles, serviceName);
-
-    if (seFile === undefined) {
-      throw new CliError(`Service "${serviceName}" not found`);
-    } else {
-      await buildServiceServerlessTemplate(frFile, seFile);
-    }
+    const fr = await loadFrameworkSchemas(getOption(tb, "schema"));
+    await buildService(fr, requireParameters(tb, "service-name")[0]);
   },
 };
 
