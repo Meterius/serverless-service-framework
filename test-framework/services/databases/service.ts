9e0d2ef623930b0a1ac54bea306dec0f954ebedd
Jonah Hoff <>
2019-11-13 12:33:20 +0100

    moved serverless template into separate file for database service

--- test-framework/services/databases/service.ts
+++ test-framework/services/databases/service.ts
@@ -1,10 +1,9 @@
 import { ServiceSchema } from "serverless-service-framework";
+import template from "./serverless-template";
 
 export default new ServiceSchema({
   name: "databases",
   shortName: "dbs",
 
-  template: {
-
-  }
+  template,
 })
