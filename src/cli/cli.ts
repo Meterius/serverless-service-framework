015919b7e08e10c0c9db6681cfbcb246374ed082
Jonah Hoff <>
2019-11-13 14:15:42 +0100

    changed cli root structure

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -2,25 +2,21 @@ import { build, GluegunCommand, GluegunToolbox } from "gluegun";
 import chalk from "chalk";
 import { CliError } from "./utility/exceptions";
 
-export async function run(argv: any): Promise<GluegunToolbox> {
-  const help: GluegunCommand = {
-    name: "help",
-    alias: "h",
-    description: "Displays Help Page",
-    run: async (tb: GluegunToolbox): Promise<void> => {
-      tb.print.printHelp(tb);
-    },
-  };
+const help: GluegunCommand = {
+  name: "help",
+  alias: "h",
+  description: "Displays Help Page",
+  run: async (tb: GluegunToolbox): Promise<void> => {
+    tb.print.printHelp(tb);
+  },
+};
 
-  const cli = build("ssf")
-    .src(__dirname)
-    .help(help)
-    .create();
+const cli = build("ssf")
+  .src(__dirname)
+  .help(help)
+  .create();
 
-  return cli.run(argv);
-}
-
-run(process.argv).then(
+cli.run(process.argv).then(
   () => {},
   (err) => {
     if (err instanceof CliError) {
