561b9021756946ff9ae19f1213ff383a4ca23d2d
Jonah Hoff <Meterius@users.noreply.github.com>
2019-11-22 14:10:50 +0100

    made framework not check typing of imported files

--- src/common/require.ts
+++ src/common/require.ts
@@ -1,11 +1,13 @@
+import { register } from "ts-node";
+
+register({
+  transpileOnly: true,
+});
+
 /**
  * Imports and returns Javascript or Typescript module export via node require.
  */
 export function requireModule(filePath: string): unknown {
-  // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
-  require("ts-node/register"); // required to transform typescript files
-
-  // eslint-disable-next-line max-len
-  // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires, import/no-dynamic-require
+  // eslint-disable-next-line global-require,import/no-dynamic-require
   return require(filePath) as unknown;
 }
