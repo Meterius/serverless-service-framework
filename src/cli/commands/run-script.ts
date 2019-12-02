8a3716406eb69d68a8c62268c12c2c7b64558b36
Jonah Hoff <>
2019-12-02 12:31:16 +0100

    added run script command to run scripts with the framework context

---
+++ src/cli/commands/run-script.ts
@@ -0,0 +1,28 @@
+import { GC, TB } from "../cli-types";
+import { requireParameters } from "../utility/options-handling";
+import { setupFrameworkContextFunction } from "../utility/command-setup";
+import { loadTypescriptModule } from "../../common/module-loading";
+import { isObject } from "../../common/type-guards";
+import { CliError } from "../utility/exceptions";
+
+const runScriptCommand: GC = {
+  name: "run-script",
+  description: "Runs a function of a typescript script and gives it an initialized framework instance",
+  run: async (tb: TB): Promise<void> => {
+    const [tsFile, tsExportName] = requireParameters(tb, ["ts-file-path", "ts-export-name"]);
+
+    const framework = await setupFrameworkContextFunction(tb);
+
+    const tsModule = await loadTypescriptModule(framework.resolvePath(tsFile), framework.schema.options);
+
+    const tsExport = isObject(tsModule) ? tsModule[tsExportName] : undefined;
+
+    if (!(tsExport instanceof Function)) {
+      throw new CliError(`TS Script "${tsFile}" does not export function at export "${tsExportName}"`);
+    }
+
+    await tsExport(framework);
+  },
+};
+
+export default runScriptCommand;
