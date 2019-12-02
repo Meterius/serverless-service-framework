8fb4ff6c671005a7c2b982e4190bcf8d08366572
Jonah Hoff <>
2019-12-02 12:09:12 +0100

    changed what is exported in lib

--- src/framework/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./aws";
-export * from "./framework-definition";
-export * from "./framework-options";
-export * from "./templates";
--- src/lib.ts
+++ src/lib.ts
@@ -1,1 +1,9 @@
-export * from "./framework";
+export * from "./framework/aws";
+
+export {
+  FrameworkTemplate, ServiceTemplate, ServerlessTemplate, ServerlessProviderName,
+} from "./framework/templates";
+export * from "./framework/service-definition";
+export * from "./framework/framework-definition";
+export * from "./framework/framework-options";
+export * from "./framework/framework";
