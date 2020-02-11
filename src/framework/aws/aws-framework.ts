24a5482515c88c537cadbd735014754dd55fec10
Jonah Hoff <Meterius@users.noreply.github.com>
2020-02-11 12:42:48 +0100

    added with changed stage to AwsFramework

--- src/framework/aws/aws-framework.ts
+++ src/framework/aws/aws-framework.ts
@@ -6,6 +6,15 @@ import { awsBaseParameter } from "./aws-base-parameter";
 import { AwsServiceDefinition } from "./aws-service-definition";
 
 export class AwsFramework extends AbstractFramework<AwsProviderDefinition> {
+  private readonly awsFrameworkConstructorParams: {
+    dirPath: string;
+    props: AwsFrameworkSchemaProperties;
+    options: FrameworkOptions;
+    serviceDefinitions: AwsServiceDefinition[];
+    stage: string;
+    profile?: string;
+  };
+
   constructor(
     dirPath: string,
     props: AwsFrameworkSchemaProperties,
@@ -16,6 +25,25 @@ export class AwsFramework extends AbstractFramework<AwsProviderDefinition> {
   ) {
     super(
       awsBaseParameter, dirPath, props, options, serviceDefinitions, stage, profile,
+    );
+
+    this.awsFrameworkConstructorParams = {
+      dirPath, props, options, serviceDefinitions, stage, profile,
+    };
+  }
+
+  /**
+   * Creates new AwsFramework that uses a different stage.
+   * @param stage
+   */
+  public withChangedStage(stage: string): AwsFramework {
+    return new AwsFramework(
+      this.awsFrameworkConstructorParams.dirPath,
+      this.awsFrameworkConstructorParams.props,
+      this.awsFrameworkConstructorParams.options,
+      this.awsFrameworkConstructorParams.serviceDefinitions,
+      stage,
+      this.awsFrameworkConstructorParams.profile,
     );
   }
 }
